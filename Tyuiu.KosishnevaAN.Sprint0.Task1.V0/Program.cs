3079922a0b1a7d45640c94d329299e0cd06320c8
Tyuiu.KosishnevaAN@outlook.com <nastyakosishneva@gmail.com>
2023-09-11 23:02:57 +0500

    Task1

--- Tyuiu.KosishnevaAN.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.KosishnevaAN.Sprint0.Task1.V0/Program.cs
@@ -10,10 +10,10 @@ namespace Tyuiu.KosishnevaAN.Sprint0.Task1.V0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
-            string name = "Игорь";
-            int age = 45;
-            Console.WriteLine("Здавствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.WriteLine("Hello, World!");
+            string name = "Настя";
+            int age = 18;
+            Console.WriteLine("Здавствуйте меня зовут " + name + " мне " + age + " лет.");
             Console.ReadKey();
         }
     }
